d3e640c3594325793830364f61946db1728f6cf9
changxiaokun <939083230@qq.com>
2021-08-23 22:36:42 +0800

    8.23-晚-自定义rest_framework_api_view

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import LoginView
+from .views import LoginView, UserAPIView
 
+from .views import user_api, user_api_detail
 
 app_name = 'user'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('api/', user_api, name='api'),
+    path('api_1/', UserAPIView.as_view(), name='api_1'),
+    path('api/<int:pk>', user_api_detail, name='api_detail'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
 from .models import UserModel
+
+from rest_framework.decorators import api_view
+
+from api.user import UserModelSerializer
 
 
 class LoginView(View):
@@ -30,3 +38,61 @@ class LoginView(View):
                 error_msg = '用户名不存在，请先注册'
 
         return render(request, 'login.html', locals())
+
+
+@api_view(['GET', 'POST'])
+def user_api(request):
+    method = request.method
+    if method == 'GET':
+        datas = UserModel.objects.all()
+        serializer = UserModelSerializer(datas, many=True)
+
+        return Response(serializer.data)
+    else:
+        serializer = UserModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.instance)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_api_detail(request, pk):
+    method = request.method
+    instance = UserModel.objects.get(pk=pk)
+    if method == 'GET':
+
+        serializer = UserModelSerializer(instance)
+        return Response(serializer.data)
+    elif method == 'PUT':
+        serializer = UserModelSerializer(instance, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(instance, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if method == 'DELETE':
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(APIView):
+    def get(self, request):
+        datas = UserModel.objects.all()
+        serializer = UserModelSerializer(datas, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.instance)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPIView(APIView):
+    pass
